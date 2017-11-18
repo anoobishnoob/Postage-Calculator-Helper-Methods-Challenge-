2fc2b8c83035cbd47c9c13767157f0aa40f0181e
Samme Qandil <Saqandil@gmail.com>
2017-11-17 21:30:52 -0800

    Add files via upload

---
+++ postageCalculatorHelperMethodsChallenge/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="postageCalculatorHelperMethodsChallenge.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <h3>Postal Calculator</h3>
+            <br />
+            <br />
+            Width:<asp:TextBox ID="widthTextBox" runat="server" AutoPostBack="True" OnTextChanged="widthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Height:<asp:TextBox ID="heightTextBox" runat="server" AutoPostBack="True" OnTextChanged="heightTextBox_TextChanged"></asp:TextBox>
+            <br />
+            Length:<asp:TextBox ID="lengthTextBox" runat="server" AutoPostBack="True" OnTextChanged="lengthTextBox_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            <br />
+            <asp:RadioButton ID="groundRadioButton" runat="server" GroupName="Shipping" Text="Ground" AutoPostBack="True" OnCheckedChanged="groundRadioButton_CheckedChanged" />
+            <br />
+            <asp:RadioButton ID="airRadioButton" runat="server" GroupName="Shipping" Text="Air" AutoPostBack="True" OnCheckedChanged="airRadioButton_CheckedChanged" />
+            <br />
+            <asp:RadioButton ID="nextDayRadioButton" runat="server" GroupName="Shipping" Text="Next Day" AutoPostBack="True" OnCheckedChanged="nextDayRadioButton_CheckedChanged" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ postageCalculatorHelperMethodsChallenge/WebForm1.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace postageCalculatorHelperMethodsChallenge
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+
+        }
+        protected void lengthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void widthTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void heightTextBox_TextChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+        protected void nextDayRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void groundRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+        protected void airRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            performChanged();
+        }
+
+
+        private void performChanged()
+        {
+            // do values exist in textboxes and checkboxes exists
+            if (!valuesExist()) return;
+
+            // volume of package 
+            double volume = 0.0;
+            if (!tryGetVolume(out volume)) return;
+
+            // what is the multiplier
+            double postageMultiplier = getPostageMultiplier();
+
+
+            // determine cost
+            double cost = volume * postageMultiplier;
+
+
+
+            // show user
+            resultLabel.Text = String.Format("The cost is {0} to ship to you", cost);
+
+            //throw new NotImplementedException();
+        }
+
+
+        private bool valuesExist()
+        {
+             // basic code structrue built. fix this is a bit
+                if (!airRadioButton.Checked
+                    && !groundRadioButton.Checked
+                    && !nextDayRadioButton.Checked)
+                    return false;
+
+                if (widthTextBox.Text.Trim().Length == 0 ||
+                    heightTextBox.Text.Trim().Length == 0) 
+                   // lengthTextBox.Text.Trim().Length == 0
+                    return false;
+
+            return true;
+                // fixed
+                // actually broken now
+               
+            
+        }
+
+        private bool tryGetVolume(out double volume)
+        {
+            //throw new NotImplementedException();
+            volume = 0;
+            double width = 0.0;
+            double height = 0.0;
+            double length = 0.0;
+
+            if (!double.TryParse(widthTextBox.Text.Trim(), out width)) return false;
+            if (!double.TryParse(heightTextBox.Text.Trim(), out height)) return false;
+            if (!double.TryParse(lengthTextBox.Text.Trim(), out length)) return false;
+
+            volume = width * height * length;
+            return true;
+
+
+        }
+
+        private double getPostageMultiplier()
+        {
+            //throw new NotImplementedException();
+            if (groundRadioButton.Checked) return .15;
+            else if (airRadioButton.Checked) return .25;
+            else if (nextDayRadioButton.Checked) return .45;
+            else return 0;
+
+                
+        }
+
+        /*private void calculateCups()
+            {
+                if (quantityTextBox.Text.Trim().Length == 0)
+                    return;
+
+                double quantity = 0.0;
+                if (!Double.TryParse(quantityTextBox.Text, out quantity))
+                    return;
+
+                double cups = 0.0;
+
+                if (fromCupsRadio.Checked) cups = quantity;
+                else if (fromPintsRadio.Checked) cups = quantity * 2;
+                else if (fromQuartsRadio.Checked) cups = quantity * 4;
+                else if (fromGallonsRadio.Checked) cups = quantity * 8;
+
+                resultLabel.Text = "The number of cups:" + cups.ToString();
+            }*/
+
+        // the above is just there to help you get the syntax right
+    }
+}
